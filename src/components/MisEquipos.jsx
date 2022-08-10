48b3576e1c0a47746f459e6a441fe01c4865a645
Daniel Dorante <dandorsan@alum.us.es>
2022-08-10 23:02:29 +0200

    create: Se ha creado el método encontrarCompañeros

--- src/components/MisEquipos.jsx
+++ src/components/MisEquipos.jsx
@@ -1,15 +1,81 @@
 import { useState, useEffect } from "react";
-import { Box, Flex, Text, Spacer, Heading } from "@chakra-ui/react"
+import {
+  Box, Flex, Text, Spacer, Heading, Button, Select, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, useDisclosure
+} from "@chakra-ui/react"
+import { BsClipboardPlus, BsPersonPlus } from "react-icons/bs"
+import { RiUserSearchFill } from "react-icons/ri"
 import * as API from "../services/equipos";
 import Navbar from './Navbar';
 
+
 export function MisEquipos() {
 
+  const API_URL = 'https://t-planifica.herokuapp.com'
   const [equipos, setEquipos] = useState([]);
+  const [data, setData] = useState([]);
+  const [managers, setManagers] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   useEffect(() => {
     API.getMisEquipos().then(setEquipos);
   }, []);
+
+
+  async function getEncontrarCompaneros(equipoId) {
+    const jwt = window.sessionStorage.getItem('jwt');
+    const myHeader = new Headers({
+      "Authorization": `Bearer ${jwt}`
+    });
+
+    const myInit = {
+      method: 'GET',
+      headers: myHeader,
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    const myRequest = new Request(`${API_URL}/` + equipoId + '/partners', myInit);
+    try {
+      await fetch(myRequest)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+        });
+    } catch (error) {
+    }
+  }
+
+  async function encontrarManagers(idEquipo) {
+    const jwt = window.sessionStorage.getItem('jwt');
+    const myHeader = new Headers({
+      "Authorization": `Bearer ${jwt}`
+    });
+
+    const myInit = {
+      method: 'GET',
+      headers: myHeader,
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    const myRequest = new Request(`${API_URL}/` + idEquipo + '/managers', myInit);
+    try {
+      await fetch(myRequest)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setManagers(json);
+        });
+    } catch (error) {
+    }
+  }
 
   return (
     <>
@@ -56,6 +122,48 @@ export function MisEquipos() {
                   </text>
                 ))}
               </Text>
+
+              <Button onClick={() => { getEncontrarCompaneros(equipo.id); onOpen(); }} > <font size="6"><RiUserSearchFill /></font> Compañeros</Button>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Compañeros</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    {data.map((item) => (
+                      <li key={item.id} >
+                        {item.nombre} {item.apellidos}
+                      </li>
+                    ))}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme='yellow' mr={3} onClick={onClose}>
+                      Cerrar
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+              <Spacer/>
+              {/* <Button onClick={() => { encontrarManagers(equipo.id); onOpen(); }} > <font size="6"><RiUserSearchFill /></font> Managers</Button>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Managers</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    {managers.map((item) => (
+                      <li key={item.id} >
+                        {item.nombre} {item.apellidos}
+                      </li>
+                    ))}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme='yellow' mr={3} onClick={onClose}>
+                      Cerrar
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal> */}
               <Spacer />
             </Flex>
           </Box>
